0db44a0791fac5bcbf09717e929e71a26eeb2d35
Si Parker <si@plogic.co.uk>
2020-01-08 10:33:37 +0000

    added: setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+long_description = """An ETL (Extract, Transform, Load) framework."""
+
+setuptools.setup(
+    name="ayeaye",
+    version="0.0.1",
+    author="Si Parker",
+    author_email="si@plogic.co.uk",
+    description="ETL framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aye-Aye-Dev/AyeAye",
+    packages=['ayeaye'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'ndjson',
+        ],
+)
