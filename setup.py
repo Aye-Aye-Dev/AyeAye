6213c6efd62943f343c8126e6a7d06ff3688e629
Si Parker <si@plogic.co.uk>
2020-01-14 17:03:54 +0000

    fixed: missing packages in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Aye-Aye-Dev/AyeAye",
-    packages=['ayeaye'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
