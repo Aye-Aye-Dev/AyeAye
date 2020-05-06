1b0fb4e36c528910e41d1010b254a9db1bf76bcb
Si Parker <si@plogic.co.uk>
2020-05-06 11:25:52 +0100

    updated: ModelsConnector construction removed: out of scope dependency idea

--- ayeaye/connectors/models_connector.py
+++ ayeaye/connectors/models_connector.py
@@ -1,4 +1,7 @@
+# import ayeaye
 from ayeaye.connectors.base import BaseConnector
+from ayeaye.model import Model
+from inspect import isclass
 
 
 class ModelsConnector(BaseConnector):
@@ -10,7 +13,28 @@ class ModelsConnector(BaseConnector):
         For args: @see :class:`connectors.base.BaseConnector`
 
         additional args for ModelsConnector
-         None
+         models (class, list, set or callable) all of which are :class:`ayeaye.Models`. They aren't
+                    instances of the class but the class itself.
         """
         super().__init__()
-        self.models = models
+
+        invalid_construction_msg = ("models must (class, list, set or callable). All of which "
+                                    "result in one or more :class:`ayeaye.Models` classes (not "
+                                    "instances)."
+                                    )
+
+        # validate and prepare
+        if isclass(models) and issubclass(models, Model):
+            models = [models, ]
+
+        if callable(models):
+            # TODO
+            raise NotImplementedError("TODO")
+
+        elif isinstance(models, (list, set)):
+            if not all([isclass(m) and issubclass(m, Model) for m in models]):
+                raise ValueError(invalid_construction_msg)
+            self.models = models
+
+        else:
+            raise ValueError(invalid_construction_msg)
--- tests/test_connectors_models.py
+++ tests/test_connectors_models.py
@@ -1,17 +1,55 @@
 import unittest
 
 import ayeaye
+from ayeaye.connectors.models_connector import ModelsConnector
 
 
 class SimpleModel(ayeaye.Model):
     place_holder = "hello models"
 
 
+class One(ayeaye.Model):
+    pass
+
+
+class Two(ayeaye.Model):
+    pass
+
+
+class Three(ayeaye.Model):
+    pass
+
+
 class TestModelConnectors(unittest.TestCase):
 
     def test_single_standalone_model(self):
         c = ayeaye.Connect(models=SimpleModel)
-        msg = ("Attrib access should be proxied through Connect to an instance of ModelsConnector "
-               "which should refer back to the Connect instance that created it."
+        msg = ("Attribute access should be proxied through Connect to an instance of "
+               "ModelsConnector which should refer back to the Connect instance that created it."
                )
-        self.assertEqual(c, c._connect_instance, msg=msg)
+        self.assertEqual(c, c.connect_instance, msg=msg)
+        self.assertEqual(c.models, [SimpleModel], "Single model should be proxied through Connect.")
+
+    def test_construction(self):
+        """
+        valid ways to make a ModelsConnector instance.
+        """
+        m = ModelsConnector(models=One)
+        self.assertIsInstance(m.models, list, "Single model becomes run-list")
+
+        m = ModelsConnector(models=[One, Two, Three])
+        self.assertIsInstance(m.models, list, "Preserves list")
+
+        m = ModelsConnector(models=[One, Two, Three])
+        self.assertIsInstance(m.models, list, "Preserves set")
+
+        with self.assertRaises(ValueError):
+            # non-model
+            m = ModelsConnector(models=[One, Two, Three()])
+
+        def models_choosen_at_runtime():
+            return set([One, Two])
+
+        with self.assertRaises(NotImplementedError):
+            # TODO
+            m = ModelsConnector(models=models_choosen_at_runtime)
